281ceb9152739c81ba926605769442f2b530685d
MEURANT <alexandremeurant@hotmail.fr>
2017-10-13 13:57:46 +0200

    Refactoring creating a new class

--- Android_Basics/app/src/main/java/com/example/android/funfacts/MainActivity.java
+++ Android_Basics/app/src/main/java/com/example/android/funfacts/MainActivity.java
@@ -18,27 +18,14 @@ public class MainActivity extends AppCompatActivity {
         // Declare and assign our View variables:
         final TextView factTextView = (TextView) findViewById(R.id.factTextView);
         Button showFactButton = (Button) findViewById(R.id.showFactButton);
+        // Instantiate FactBook object to return a random fact:
+        final FactsBook factsBook = new FactsBook();
 
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // List of facts:
-                String[] facts = {
-                        "Ants stretch when they wake up in the morning.",
-                        "Ostriches can run faster than horses.",
-                        "Olympic gold medals are actually made mostly of silver.",
-                        "You are born with 300 bones; by the time you are an adult you will have 206.",
-                        "It takes about 8 minutes for light from the Sun to reach Earth.",
-                        "Some bamboo plants can grow almost a meter in just one day.",
-                        "The state of Florida is bigger than England.",
-                        "Some penguins can leap 2-3 meters out of the water.",
-                        "On average, it takes 66 days to form a new habit.",
-                        "Mammoths still walked the earth when the Great Pyramid was being built." };
-
                 // When button is clicked, the fact textView shows a fact randomly from the facts'list:
-                Random random = new Random();
-                int randomNumber = random.nextInt(facts.length);
-                String fact = facts[randomNumber];
+                String fact = factsBook.getFact();
                 factTextView.setText(fact);
             }
         };
