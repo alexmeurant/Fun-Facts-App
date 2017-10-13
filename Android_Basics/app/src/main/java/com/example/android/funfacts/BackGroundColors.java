544e1a86ed5615e02800b08647a7e5e93a14efeb
MEURANT <alexandremeurant@hotmail.fr>
2017-10-13 14:28:01 +0200

    added new class to change background color

---
+++ Android_Basics/app/src/main/java/com/example/android/funfacts/BackGroundColors.java
@@ -0,0 +1,30 @@
+package com.example.android.funfacts;
+
+
+import java.util.Random;
+
+public class BackGroundColors {
+    // Member variable:
+    public String[] colors = {
+            "#39add1", // light blue
+            "#3079ab", // dark blue
+            "#c25975", // mauve
+            "#e15258", // red
+            "#f9845b", // orange
+            "#838cc7", // lavender
+            "#7d669e", // purple
+            "#53bbb4", // aqua
+            "#51b46d", // green
+            "#e0ab18", // mustard
+            "#637a91", // dark gray
+            "#f092b0", // pink
+            "#b7c0c7"  // light gray
+    };
+
+    // Method:
+    public String getColor(){
+        Random random = new Random();
+        int randomNumber = random.nextInt(colors.length);
+        return colors[randomNumber];
+    }
+}
--- Android_Basics/app/src/main/java/com/example/android/funfacts/MainActivity.java
+++ Android_Basics/app/src/main/java/com/example/android/funfacts/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.android.funfacts;
 
+import android.graphics.Color;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.Random;
+
+import static com.example.android.funfacts.R.id.constraintLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,9 +22,14 @@ public class MainActivity extends AppCompatActivity {
 
         // Declare and assign our View variables:
         final TextView factTextView = (TextView) findViewById(R.id.factTextView);
-        Button showFactButton = (Button) findViewById(R.id.showFactButton);
+        final Button showFactButton = (Button) findViewById(R.id.showFactButton);
+        final ConstraintLayout constraintLayout = (ConstraintLayout) findViewById(R.id.constraintLayout);
+
         // Instantiate FactBook object to return a random fact:
         final FactsBook factsBook = new FactsBook();
+
+        // Instantiate BackGroundColors object to display a random color:
+        final BackGroundColors backGroundColor = new BackGroundColors();
 
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
@@ -27,6 +37,12 @@ public class MainActivity extends AppCompatActivity {
                 // When button is clicked, the fact textView shows a fact randomly from the facts'list:
                 String fact = factsBook.getFact();
                 factTextView.setText(fact);
+
+                // and background color changed:
+                String bgColor = backGroundColor.getColor();
+                int color = Color.parseColor(bgColor);
+                constraintLayout.setBackgroundColor(color);
+                showFactButton.setTextColor(color);
             }
         };
         showFactButton.setOnClickListener(listener);
